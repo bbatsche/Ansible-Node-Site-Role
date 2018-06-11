dd152809b65bc431aa754464355d743240fdd6f4
Ben Batschelet <bartleby84@gmail.com>
2018-06-11 14:18:24 -0500

    Update path to bootstrap; update task include syntax

--- spec/node-spec.rb
+++ spec/node-spec.rb
@@ -1,4 +1,4 @@
-require_relative "bootstrap"
+require_relative "lib/bootstrap"
 
 RSpec.configure do |config|
   config.before :suite do
@@ -15,11 +15,11 @@ end
 
 describe "NVM" do
   let(:subject) { command("nvm --version") }
-  
+
   it "is installed" do
     expect(subject.stdout).to match /^\d+\.\d+\.\d+$/
   end
-  
+
   # Can't use "no errors"; docker thinks this should be interactive/login shell
   it "has no errors" do
     expect(subject.exit_status).to eq 0
@@ -29,11 +29,11 @@ end
 context "CLI" do
   describe "Node runtime" do
     let(:subject) { command(%Q{nvm run 9.4.0 -e "console.log('node is installed');"}) }
-    
+
     it "executes JavaScript" do
       expect(subject.stdout).to match /^node is installed$/
     end
-    
+
     it "has no errors" do
       expect(subject.exit_status).to eq 0
     end
@@ -41,11 +41,11 @@ context "CLI" do
 
   describe "Node version" do
     let(:subject) { command("nvm run 9.4.0 --version") }
-    
+
     it "is the requested version" do
       expect(subject.stdout).to match /^#{Regexp.quote("v9.4.0")}$/
     end
-    
+
     it "has no errors" do
       expect(subject.exit_status).to eq 0
     end
@@ -54,11 +54,11 @@ end
 
 describe "Web requests" do
   let(:subject) { command("curl -i node-test.dev") }
-  
+
   include_examples("curl request", "200")
 
   include_examples "curl request html"
-  
+
   it "processed Node.js code" do
     expect(subject.stdout).to match /Phusion Passenger is serving Node.js v9\.4\.0 code on node-test\.dev/
   end
